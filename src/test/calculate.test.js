a70af0fa9243262026cd1222d85dc1a238bec061
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-04 01:30:55 +0430

    Add 3 tests for (.) button

--- src/test/calculate.test.js
+++ src/test/calculate.test.js
@@ -1,20 +1,40 @@
-import calculate from "../logic/calculate";
+import calculate from '../logic/calculate';
 
-describe("Calculate tests", () => {
-  it("ordinary key", () => {
+describe('Calculate tests', () => {
+  it('ordinary key', () => {
     expect(
-      calculate({ total: null, next: null, operation: null }, "9")
+      calculate({ total: null, next: null, operation: null }, '9'),
     ).toEqual({
-      next: "9",
+      next: '9',
       total: null,
     });
   });
-  it("ordinary key", () => {
+  it('ordinary key', () => {
     expect(
-      calculate({ total: null, next: null, operation: null }, "0")
+      calculate({ total: null, next: null, operation: null }, '0'),
     ).toEqual({
-      next: "0",
+      next: '0',
       total: null,
     });
+  });
+  it('AC key', () => {
+    expect(calculate({ total: '9', next: '12', operation: '-' }, 'AC')).toEqual(
+      { total: null, next: null, operation: null },
+    );
+  });
+  it('percentage key test 1', () => {
+    expect(
+      calculate({ total: null, next: '3.1', operation: null }, '.'),
+    ).toEqual({ total: null, next: '3.1', operation: null });
+  });
+  it('percentage key test 2', () => {
+    expect(
+      calculate({ total: null, next: '11', operation: null }, '.'),
+    ).toEqual({ total: null, next: '11.', operation: null });
+  });
+  it('percentage key test 3', () => {
+    expect(calculate({ total: null, next: null, operation: '+' }, '.')).toEqual(
+      { next: '0.' },
+    );
   });
 });
