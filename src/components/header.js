c12f571ac9a6720bb9107c45b87b0a974b303e97
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-11-03 16:01:33 +0200

    Add Mobile responsive with Bootstrap

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <div className="header">
-    <nav className="left">
-      <h1 className="sitename">Math Magicians</h1>
-    </nav>
-    <nav className="right">
-      <ul className="nabar">
-        <li className="list-item"><Link style={{ textDecoration: 'none' }} to="/Home">Home</Link></li>
-        <li className="list-item"><Link style={{ textDecoration: 'none' }} to="/Calculator">Calculator</Link></li>
-        <li className="list-item"><Link style={{ textDecoration: 'none' }} to="/Quote">Quote</Link></li>
-      </ul>
+  <div data-testid="NavMenu">
+    <nav className="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
+      <div className="container-fluid">
+        <Link className="navbar-brand header" to="/">Math Magicians</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse me-auto" id="navbarNavAltMarkup">
+          <div className="navbar-nav ms-auto text-light">
+            <Link to="/Home" className="nav-link list-item">Home</Link>
+            <Link to="/Calculator" className="nav-link list-item">Calculator</Link>
+            <Link to="/Quote" className="nav-link list-item">Quote</Link>
+          </div>
+        </div>
+      </div>
     </nav>
   </div>
 );
