4c854bdff383146289b0ab223b0035353f4d6e0e
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-04 02:41:09 +0430

    Add tests for user interaction simulation

--- src/test/components.test.js
+++ src/test/components.test.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+
+// components
 import Home from '../components/Home';
 import Calculator from '../components/Calculator';
 import Quote from '../components/Quote';
@@ -24,5 +27,17 @@ describe('Components Matches Test', () => {
       .create(<Quote />)
       .toJSON();
     expect(quote).toMatchSnapshot();
+  });
+});
+
+describe('User interaction simulation test', () => {
+  it('Home route test', () => {
+    render(<Home />);
+  });
+  it('Quote route test', () => {
+    render(<Quote />);
+  });
+  it('Calculator route test', () => {
+    render(<Calculator />);
   });
 });
